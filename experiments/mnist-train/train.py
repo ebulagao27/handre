cfa08e710ed0bb0666dbfbd9eb1621db883e0f49
lim.chong.u <lim.chong.u@gmail.com>
2011-11-26 06:31:06 +0000

    training works now :), though I had to limit it to 5 digits because the training was taking way too long

--- experiments/mnist-train/train.py
+++ experiments/mnist-train/train.py
@@ -6,7 +6,7 @@ from scipy.misc import imsave
 from sklearn import svm, metrics
 
 # train the digits 0
-digit = [0]
+digit = [1,2,3,4,5]
 
 # read the training data and labelsi
 sys.stdout.write('Reading in training data and labels')
@@ -20,12 +20,24 @@ sys.stdout.write(' ... Done!\n')
 
 # convert to arrays
 x_train = array(train_images)
-y_train = array(train_labels)
+y_train = array(train_labels).reshape(1, len(train_labels))[0]
+
+x_test = array(test_images)
+y_test = array(test_labels).reshape(1, len(test_labels))[0]
 
 # create classifier
-classifier = svm.SVC()
+classifier = svm.LinearSVC()
 
 # train the classifier
 sys.stdout.write('Training the classifier')
 classifier.fit(x_train, y_train)
-sys.stdout.write(' ... Done!')
+sys.stdout.write(' ... Done!\n')
+
+# test the classifier
+sys.stdout.write("Performing prediction on test set")
+prediction = classifier.predict(x_test);
+sys.stdout.write(" ... Done!\n")
+
+# report
+print 'Classification report for classifier [%s]\n%s' % (classifier, metrics.classification_report(y_test, prediction))
+print 'Confusion matrix:\n%s' % (metrics.confusion_matrix(y_test, prediction))
