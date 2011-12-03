0a3ee79e951d5125c7fba10ab8ab5ce71f18ef12
lim.chong.u <lim.chong.u@gmail.com>
2011-12-03 06:15:18 +0000

    added tests for leslie data

---
+++ experiments/leslie-test/mnist.py
@@ -0,0 +1,30 @@
+import os, struct
+from array import array
+from cvxopt.base import matrix
+
+def read(digits, path = "./../../data/"):
+    """
+    Python function for importing the MNIST data set.
+    """
+
+    fname_img = os.path.join(path, 'alphabets-font-images.idx')
+    fname_lbl = os.path.join(path, 'alphabets-font-labels.idx')
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = array("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = array("B", fimg.read())
+    fimg.close()
+
+    ind = [ k for k in xrange(size) if (lbl[k] in digits or digits == []) ]
+    images =  matrix(0, (len(ind), rows*cols))
+    labels = matrix(0, (len(ind), 1))
+    for i in xrange(len(ind)):
+        images[i, :] = img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
