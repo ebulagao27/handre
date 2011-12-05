ef4d4ac404aadf51a102cf6ef0edb43cc73d5db2
lim.chong.u@gmail.com <lim.chong.u@gmail.com@a03e8917-9592-1cf5-aed0-23fa43096e21>
2011-12-05 16:28:47 +0000

    trained model only for lower case

--- trunk/experiments/combined/train.py
+++ trunk/experiments/combined/train.py
@@ -12,7 +12,7 @@ from numpy import vstack
 from numpy import append
 import os
 # train the digits 0
-alphabets  = [] #'a', 'd', 'e', 'c', 'u', 's', 'o', 'f', 't']
+alphabets  = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #'a', 'd', 'e', 'c', 'u', 's', 'o', 'f', 't']
 alphabets_ord = map(ord, alphabets)
 ignoreList = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
 ignoreList = map(ord, ignoreList)
